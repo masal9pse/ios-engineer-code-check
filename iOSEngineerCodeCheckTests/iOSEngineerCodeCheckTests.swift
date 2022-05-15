3b76b7e3c80ded56a18788cadb23b838f2996d6a
masal9pse <returnymgstokh@icloud.com>
2022-05-16 05:19:24 +0900

    swiftFormatでインデントの自動修正

--- iOSEngineerCodeCheckTests/iOSEngineerCodeCheckTests.swift
+++ iOSEngineerCodeCheckTests/iOSEngineerCodeCheckTests.swift
@@ -10,7 +10,6 @@ import XCTest
 @testable import iOSEngineerCodeCheck
 
 class iOSEngineerCodeCheckTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -30,5 +29,4 @@ class iOSEngineerCodeCheckTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
