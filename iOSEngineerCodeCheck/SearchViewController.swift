033910b12c56717fe4035b9d2354740638e2ee3d
masal9pse <returnymgstokh@icloud.com>
2022-05-17 08:14:00 +0900

    #01 サーチバーの変数名を変更

--- iOSEngineerCodeCheck/SearchViewController.swift
+++ iOSEngineerCodeCheck/SearchViewController.swift
@@ -1,20 +1,20 @@
 import UIKit
 
 class SearchViewController: UITableViewController, UISearchBarDelegate {
-    @IBOutlet weak var SchBr: UISearchBar!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var repo: [[String: Any]] = []
     
     var task: URLSessionTask?
-    var word: String!
+    var searchedWord: String!
     var url: String!
-    var idx: Int!
+    var index: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        SchBr.text = "GitHubのリポジトリを検索できるよー"
-        SchBr.delegate = self
+        searchBar.text = "GitHubのリポジトリを検索できるよー"
+        searchBar.delegate = self
     }
     
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
@@ -28,13 +28,13 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        word = searchBar.text!
+        searchedWord = searchBar.text!
         
-        if word.count != 0 {
-            url = "https://api.github.com/search/repositories?q=\(word!)"
+        if searchedWord.count != 0 {
+            url = "https://api.github.com/search/repositories?q=\(searchedWord!)"
             task = URLSession.shared.dataTask(with: URL(string: url)!) { data, res, err in
-                if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
-                    if let items = obj["items"] as? [[String: Any]] {
+                if let data = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
+                    if let items = data["items"] as? [[String: Any]] {
                         self.repo = items
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
@@ -49,8 +49,8 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Detail" {
-            let dtl = segue.destination as! ShowViewController
-            dtl.vc1 = self
+            let detail = segue.destination as! DetailViewController
+            detail.searchViewController = self
         }
     }
     
@@ -60,16 +60,16 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        let rp = repo[indexPath.row]
-        cell.textLabel?.text = rp["full_name"] as? String ?? ""
-        cell.detailTextLabel?.text = rp["language"] as? String ?? ""
+        let repo = repo[indexPath.row]
+        cell.textLabel?.text = repo["full_name"] as? String ?? ""
+        cell.detailTextLabel?.text = repo["language"] as? String ?? ""
         cell.tag = indexPath.row
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 画面遷移時に呼ばれる
-        idx = indexPath.row
+        index = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
     }
 }
