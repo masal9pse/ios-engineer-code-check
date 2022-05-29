2934507a8c525e591021d5faf769d895600ab20b
masal9pse <returnymgstokh@icloud.com>
2022-05-29 17:21:42 +0900

    custom_object使用後に、リスト更新成功

--- iOSEngineerCodeCheck/SearchViewController.swift
+++ iOSEngineerCodeCheck/SearchViewController.swift
@@ -4,7 +4,8 @@ import Alamofire
 class SearchViewController: UITableViewController, UISearchBarDelegate {
     @IBOutlet weak var searchBar: UISearchBar!
     
-    var repo: [[String: Any]] = []
+    var items: [Item] = []
+    let gitHubApiResponse = GitHubApiRepository()
     
     var task: URLSessionTask?
     var searchedWord: String!
@@ -32,57 +33,34 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
         searchedWord = searchBar.text!
         
         if searchedWord.count != 0 {
-            url = "https://api.github.com/search/repositories?q=\(searchedWord!)"
-//            task = URLSession.shared.dataTask(with: URL(string: url)!) { data, res, err in
-            ////                if let data = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
-//                if let data = try! JSONSerialization.jsonObject(with: data!) as? [Item] {
-//                    let decode = JSONDecoder()
-            ////                    let execute = decode.decode(GitHubApiResponse.self, from: data)
-            ////                    DispatchQueue.main.async {
-            ////                        self.tableView.reloadData()
-            ////                    }
-//                    let result = try decode.decode(Item.self, from: data)
-            ////                    if let items = data["items"] as? [[String: Any]] {
-            ////                        self.repo = items
-            ////                        DispatchQueue.main.async {
-            ////                            self.tableView.reloadData()
-            ////                        }
-            ////                    }
-//                }
-//            }
-//            // これ呼ばなきゃリストが更新されません
-//            task?.resume()
-//            let header: HTTPHeaders = ["Accept": "application/x-www-form-urlencoded"]
-            Alamofire.request(url, method: .get, encoding: JSONEncoding(options: [])).responseJSON { response in
-                // debugPrint(response)
-                do {
-                    guard let data = response.data else {
-                        return
-                    }
-                    let decode = JSONDecoder()
-                    let result = try decode.decode(GitHubApiResponse.self, from: data)
-                    print(result)
-                } catch {}
-            }
+            gitHubApiResponse.getGitHubApiResponse(searchedWord: searchedWord, comp: { data in
+                self.items = data
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            })
         }
+//        task?.resume()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Detail" {
-            let detail = segue.destination as! DetailViewController
-            detail.searchViewController = self
+//            let detail = segue.destination as! DetailViewController
+//            detail.searchViewController = self
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repo.count
+//        return repo.count
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        let repo = repo[indexPath.row]
-        cell.textLabel?.text = repo["full_name"] as? String ?? ""
-        cell.detailTextLabel?.text = repo["language"] as? String ?? ""
+        let item = items[indexPath.row]
+
+        cell.textLabel?.text = item.name
+//        cell.detailTextLabel?.text = repo["language"] as? String ?? ""
         cell.tag = indexPath.row
         return cell
     }
