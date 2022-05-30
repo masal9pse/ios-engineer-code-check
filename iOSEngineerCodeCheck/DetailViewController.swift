e2bc73e95042780697d6099fd881a9184a86a2d5
masal9pse <returnymgstokh@icloud.com>
2022-05-30 23:12:33 +0900

    custom-object使用後、詳細ページの表示に成功

--- iOSEngineerCodeCheck/DetailViewController.swift
+++ iOSEngineerCodeCheck/DetailViewController.swift
@@ -9,38 +9,35 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var forksCountLable: UILabel!
     @IBOutlet weak var issuesCountLabel: UILabel!
 
+    // すでに値が入っていることが確定しているので暗黙的アンラップ型を使用
     var searchViewController: SearchViewController!
-
+    var searchedItem: Item!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        let repo = searchViewController.repo[searchViewController.index]
-        let searchItem = searchViewController.items[searchViewController.index]
+        searchedItem = searchViewController.items[searchViewController.index]
 
-//        languageLabel.text = "Written in \(repo["language"] as? String ?? "")"
-        languageLabel.text = "Written in \(searchItem.language ?? "")"
-        stargazersCountLable.text = "\(searchItem.stargazersCount) stars"
-//        watchersCountLabel.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
-//        forksCountLable.text = "\(repo["forks_count"] as? Int ?? 0) forks"
-//        issuesCountLabel.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
-//        getImage()
-        let k = 3
+        languageLabel.text = "Written in \(searchedItem.language ?? "")"
+        stargazersCountLable.text = "\(searchedItem.stargazersCount) stars"
+        watchersCountLabel.text = "\(searchedItem.watchersCount) watchers"
+        forksCountLable.text = "\(searchedItem.forksCount) forks"
+        issuesCountLabel.text = "\(searchedItem.openIssuesCount) open issues"
+        getImage()
     }
     
-//    func getImage() {
-//        let repo = searchViewController.repo[searchViewController.index]
-//
-//        titleLabel.text = repo["full_name"] as? String
-//
-//        if let owner = repo["owner"] as? [String: Any] {
-//            if let imgURL = owner["avatar_url"] as? String {
-//                URLSession.shared.dataTask(with: URL(string: imgURL)!) { data, res, err in
-//                    let img = UIImage(data: data!)!
-//                    DispatchQueue.main.async {
-//                        self.imageView.image = img
-//                    }
-//                }.resume()
-//            }
-//        }
-//    }
+    func getImage() {
+        titleLabel.text = searchedItem.fullName
+        
+        if let owner = searchedItem.owner {
+            if let imageUrl = owner.avatarUrl {
+                URLSession.shared.dataTask(with: URL(string: imageUrl)!) { data, response, error in
+                    let image = UIImage(data: data!)!
+                    DispatchQueue.main.async {
+                        self.imageView.image = image
+                    }
+                }.resume()
+            }
+        }
+    }
 }
