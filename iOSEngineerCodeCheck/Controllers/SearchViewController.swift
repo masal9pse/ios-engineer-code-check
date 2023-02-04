f63dc38b8131b8404b89627dc536603189515806
山本大翔 <masato.yamamoto@sellca-sellcar.com>
2023-02-05 04:05:49 +0900

    #52 searchPageからUITableViewDataSource, UITableViewDelegateの責務を別クラスに切り出し

--- iOSEngineerCodeCheck/Controllers/SearchViewController.swift
+++ iOSEngineerCodeCheck/Controllers/SearchViewController.swift
@@ -1,18 +1,17 @@
 import UIKit
 
-final class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+final class SearchViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet private weak var searchBar: UISearchBar!
-    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet weak var tableView: TableViewClass!
+    
     var items: [Item] = []
     let indicator = UIActivityIndicatorView()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.placeholder = "GitHubのリポジトリを検索できます"
         searchBar.delegate = self
-        tableView.dataSource = self
-        tableView.delegate = self
         tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
+        tableView.reloadTableViewWith(withData: [], withContentController: self)
         // 表示位置を設定（画面中央）
         indicator.center = view.center
         // インジケーターのスタイルを指定（白色＆大きいサイズ）
@@ -31,9 +30,8 @@ final class SearchViewController: UIViewController, UITableViewDataSource, UITab
             Task {
                 do {
                     let response = try await gitHubApiResponse.getGitHubApiResponse(searchedWord: searchedWord)
+                    tableView.reloadTableViewWith(withData: response, withContentController: self)
                     DispatchQueue.main.async {
-                        self.items = response
-                        self.tableView.reloadData()
                         self.indicator.stopAnimating()
                     }
                 } catch {
@@ -43,26 +41,5 @@ final class SearchViewController: UIViewController, UITableViewDataSource, UITab
         }
         // キーボードを下ろす。
         view.endEditing(true)
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as? TableViewCell {
-            let item = items[indexPath.row]
-            cell.setup(item: item)
-            return cell
-        }
-        return UITableViewCell()
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let index = indexPath.row
-        let storyboard = UIStoryboard(name: "DetailPage", bundle: nil)
-        let nextVC = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-        nextVC.searchedItem = self.items[index]
-        navigationController?.pushViewController(nextVC, animated: true)
     }
 }
---
+++ iOSEngineerCodeCheck/Controllers/TableViewClass.swift
@@ -0,0 +1,42 @@
+//
+//  TableViewPresenter.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 山本大翔 on 2023/02/05.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class TableViewClass: UITableView, UITableViewDataSource, UITableViewDelegate {
+    var items: [Item] = []
+    var presentController = UIViewController()
+    func reloadTableViewWith(withData: [Item], withContentController: UIViewController) {
+        items = withData
+        presentController = withContentController
+        self.dataSource = self
+        self.delegate = self
+        self.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as? TableViewCell {
+            let item = items[indexPath.row]
+            cell.setup(item: item)
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let index = indexPath.row
+        let storyboard = UIStoryboard(name: "DetailPage", bundle: nil)
+        let nextVC = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        nextVC.searchedItem = self.items[index]
+        presentController.navigationController?.pushViewController(nextVC, animated: true)
+    }
+}
