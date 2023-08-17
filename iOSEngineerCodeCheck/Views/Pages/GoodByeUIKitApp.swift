f8438c00112a9a1b57565b45d6bb7b770f8354a0
masal9pse <returnymgstokh@icloud.com>
2023-08-18 02:10:38 +0900

    リストが動作した。

--- iOSEngineerCodeCheck/Views/Pages/GoodByeUIKitApp.swift
+++ iOSEngineerCodeCheck/Views/Pages/GoodByeUIKitApp.swift
@@ -11,28 +11,16 @@ import SwiftUI
 struct GoodByeUIKitApp2: View {
     @State private var fruits = ["りんご", "オレンジ", "バナナ"]
     var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         Button("hit gitHubAPI", action: {
             fruits.append("ナシ")
-            let k = 3
-//            Task {
-//                let api = URL(string: "https://api.github.com/search/repositories?q=php")
-//                guard let api = api else {
-//                    throw WebApiError.dataNotFound
-//                }
-//                let (data, _) = try await URLSession.shared.data(from: api, delegate: nil)
-//                let response = try? JSONDecoder().decode(GitHubApiResponse.self, from: data)
-//                print(response?.items.first?.name)
-//                let o = 4
-//            }
         })
         List {
-//            Text("大根")
-//            Text("キャベツ")
-//            Text("じゃがいも")
-            ForEach(0 ..< fruits.count) { index in
+            ForEach(fruits.indices, id: \.self) { index in
                 Text(fruits[index])
             }
+//            ForEach(0 ..< fruits.count) { index in
+//                Text(fruits[index])
+//            }
         }
     }
 }
@@ -43,31 +31,6 @@ struct GoodByeUIKitApp: App {
     var body: some Scene {
         WindowGroup {
             GoodByeUIKitApp2() // 一番最初に表示されるView: これから作成します
-//            MyPage(user: User())
         }
-    }
-}
-
-// 複数画面で状態を共有
-@MainActor
-final class User: ObservableObject {
-    @Published var isLogin = false
-}
-
-struct MyPage: View {
-    @ObservedObject var user: User
-    var body: some View {
-        Button {
-            user.isLogin.toggle()
-        } label: {
-            Text(user.isLogin ? "ログアウト" : "ログイン")
-        }
-    }
-}
-
-struct TopPage: View {
-    @ObservedObject var user: User
-    var body: some View {
-        Text(user.isLogin ? "ログイン済み" : "未ログイン")
     }
 }
