20ed45c5598efe485e57cb79daddfe4c3868adbf
masal9pse <returnymgstokh@icloud.com>
2023-08-20 08:56:52 +0900

    Listで画面遷移する

--- iOSEngineerCodeCheck/Views/Pages/GoodByeUIKitApp.swift
+++ iOSEngineerCodeCheck/Views/Pages/GoodByeUIKitApp.swift
@@ -5,20 +5,39 @@
 //  Created by 山本大翔 on 2023/07/31.
 //  Copyright © 2023 YUMEMI Inc. All rights reserved.
 //
-
+// リンクを表示
+// 画面遷移のやり方
+// 詳細ページを表示
 import SwiftUI
 
 struct GoodByeUIKitApp2: View {
     @ObservedObject var stateClass: StateClass
     var body: some View {
-        Button("hit gitHubAPI", action: {
+        Button("ナシ追加", action: {
             stateClass.fruits.append("ナシ")
         })
-        List {
-            ForEach(stateClass.fruits.indices, id: \.self) { index in
-                Text(stateClass.fruits[index])
+        NavigationView {
+            List {
+                ForEach(stateClass.fruits.indices, id: \.self) { index in
+                    // 遷移しない
+                    //                NavigationView {
+                    //                    NavigationLink(destination: SecondView()) {
+                    //                        Text("画面遷移")
+                    //                    }
+                    //                }
+                    NavigationLink(destination: SecondView()) {
+                        Text("画面遷移")
+                    }
+                    Text(stateClass.fruits[index])
+                }
             }
         }
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+        Text("画面遷移")
     }
 }
 
