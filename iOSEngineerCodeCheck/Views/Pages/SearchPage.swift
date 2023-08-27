6e19d675733df3a39934a16075701211608bd4cd
masal9pse <returnymgstokh@icloud.com>
2023-08-27 09:19:38 +0900

    NavigationViewの階層を変更

--- iOSEngineerCodeCheck/Views/Pages/SearchPage.swift
+++ iOSEngineerCodeCheck/Views/Pages/SearchPage.swift
@@ -17,42 +17,19 @@ struct SearchPage: View {
     }
     
     var body: some View {
-        VStack {
-            Button("ボタン", action: {
-                Task {
-                    try await searchApiState.getGitHubApiResponse(searchedWord: "php")
-                }
-            })
-            TextField("名前を入力して下さい", text: $name,
-                      
-                      onEditingChanged: { begin in
-                          /// 入力開始処理
-                          if begin {
-                              self.editting = true // 編集フラグをオン
-                              /// 入力終了処理
-                          } else {
-                              self.editting = false // 編集フラグをオフ
-                          }
-                      },
-                      
-                      /// リターンキーが押された時の処理
-                      onCommit: {
-                          self.message = "こんにちは、\(self.name)さん" // メッセージセット
-                          self.name = "" // 入力域をクリア
-                               
-                      })
-                      .textFieldStyle(RoundedBorderTextFieldStyle()) // 入力域を枠で囲む
-                      .padding() // 余白を追加
-                      // 編集フラグがONの時に枠に影を付ける
-                      .shadow(color: editting ? .blue : .clear, radius: 3)
-                       
-        }
-        if searchApiState.apiResponseList != nil {
-            NavigationView {
-                List {
-                    ForEach(searchApiState.apiResponseList!.items.indices, id: \.self) { index in
-                        NavigationLink(destination: DetailPage(info: searchApiState.apiResponseList!.items[index].fullName)) {
-                            Text(searchApiState.apiResponseList!.items[index].fullName)
+        NavigationView {
+            VStack {
+                Button("ボタン", action: {
+                    Task {
+                        try await searchApiState.getGitHubApiResponse(searchedWord: "php")
+                    }
+                })
+                if searchApiState.apiResponseList != nil {
+                    List {
+                        ForEach(searchApiState.apiResponseList!.items.indices, id: \.self) { index in
+                            NavigationLink(destination: DetailPage(info: searchApiState.apiResponseList!.items[index].fullName)) {
+                                Text(searchApiState.apiResponseList!.items[index].fullName)
+                            }
                         }
                     }
                 }
