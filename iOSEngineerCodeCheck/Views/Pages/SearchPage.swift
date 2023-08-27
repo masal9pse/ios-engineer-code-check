a34fd61abc157e5f9e39b37c83b6d82cf3594ed8
masal9pse <returnymgstokh@icloud.com>
2023-08-27 09:13:05 +0900

    TextFieldの追加

--- iOSEngineerCodeCheck/Views/Pages/SearchPage.swift
+++ iOSEngineerCodeCheck/Views/Pages/SearchPage.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct SearchPage: View {
+    @State private var name = ""
+    @State private var editting = false
     @ObservedObject var searchApiState: SearchApiState
     
     init(searchApiRepository: SearchApiRepositoryProtocol) {
@@ -15,11 +17,36 @@ struct SearchPage: View {
     }
     
     var body: some View {
-        Button("ナシ追加", action: {
-            Task {
-                try await searchApiState.getGitHubApiResponse(searchedWord: "php")
-            }
-        })
+        VStack {
+            Button("ボタン", action: {
+                Task {
+                    try await searchApiState.getGitHubApiResponse(searchedWord: "php")
+                }
+            })
+            TextField("名前を入力して下さい", text: $name,
+                      
+                      onEditingChanged: { begin in
+                          /// 入力開始処理
+                          if begin {
+                              self.editting = true // 編集フラグをオン
+                              /// 入力終了処理
+                          } else {
+                              self.editting = false // 編集フラグをオフ
+                          }
+                      },
+                      
+                      /// リターンキーが押された時の処理
+                      onCommit: {
+                          self.message = "こんにちは、\(self.name)さん" // メッセージセット
+                          self.name = "" // 入力域をクリア
+                               
+                      })
+                      .textFieldStyle(RoundedBorderTextFieldStyle()) // 入力域を枠で囲む
+                      .padding() // 余白を追加
+                      // 編集フラグがONの時に枠に影を付ける
+                      .shadow(color: editting ? .blue : .clear, radius: 3)
+                       
+        }
         if searchApiState.apiResponseList != nil {
             NavigationView {
                 List {
