db6a7bed7ff78f28a0a0fd479c1f2081e49160ad
masal9pse <returnymgstokh@icloud.com>
2023-08-29 02:46:49 +0900

    トップページのデザイン修正

--- iOSEngineerCodeCheck/Presentation/Views/Pages/SearchPage.swift
+++ iOSEngineerCodeCheck/Presentation/Views/Pages/SearchPage.swift
@@ -17,22 +17,24 @@ struct SearchPage: View {
     }
     
     var body: some View {
-        VStack {
-            SearchFormField(action: {
-                Task {
-                    try await searchApiState.getGitHubApiResponse(searchedWord: self.name)
-                }
-            }, name: $name, editting: $editting)
-            if searchApiState.apiResponseList != nil {
-                List {
-                    ForEach(searchApiState.apiResponseList!.items.indices, id: \.self) { index in
-                        NavigationLink(destination: DetailPage(info: searchApiState.apiResponseList!.items[index].fullName)) {
-                            Text(searchApiState.apiResponseList!.items[index].fullName)
+        NavigationView {
+            VStack {
+                SearchFormField(action: {
+                    Task {
+                        try await searchApiState.getGitHubApiResponse(searchedWord: self.name)
+                    }
+                }, name: $name, editting: $editting)
+                if searchApiState.apiResponseList != nil {
+                    List {
+                        ForEach(searchApiState.apiResponseList!.items.indices, id: \.self) { index in
+                            NavigationLink(destination: DetailPage(info: searchApiState.apiResponseList!.items[index].fullName)) {
+                                Text(searchApiState.apiResponseList!.items[index].fullName)
+                            }
                         }
                     }
                 }
-            }
-            Spacer()
+                Spacer()
+            }.navigationTitle("Github検索アプリ")
         }
     }
 }
