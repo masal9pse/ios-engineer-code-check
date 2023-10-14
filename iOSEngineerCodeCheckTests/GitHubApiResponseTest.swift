ee39f317091c2129b49bce3192376cc0afade1b9
masal9pse <returnymgstokh@icloud.com>
2023-10-15 06:03:45 +0900

    テストファイルの修正

--- iOSEngineerCodeCheckTests/GitHubApiResponseTest.swift
+++ iOSEngineerCodeCheckTests/GitHubApiResponseTest.swift
@@ -19,7 +19,7 @@ class GitHubApiResponseTest: XCTestCase {
 
         XCTAssertEqual(decodeData.totalCount, 1)
         XCTAssertEqual(decodeData.items[0].name, "Tetris")
-        XCTAssertEqual(decodeData.items[0].owner?.avatarUrl, "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png")
+        XCTAssertEqual(decodeData.items[0].owner.avatarUrl, "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png")
         XCTAssertEqual(decodeData.items[0].stargazersCount, 1)
     }
 }
